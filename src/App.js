8dc1090568bc4b26fc156515b9255c15c92daee4
billlanyon <billlanyon@me.com>
2021-03-16 07:54:56 +0100

    Adds auth.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import logo from './logo.svg';
+import React from 'react';
+import logo from './BSLogo.png';
 import './App.css';
+import { withAuthenticator } from 'aws-amplify-react';
+import '@aws-amplify/ui/dist/style.css';
 
 function App() {
   return (
@@ -7,19 +10,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>beautysafety.net</code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App, {includeGreetings: true});
